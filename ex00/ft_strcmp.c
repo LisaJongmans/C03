dcadf23137ff72550e9d6fc239bc3e167e6dbae5
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-15 13:19:49 +0200

    ft_strcmp

---
+++ ex00/ft_strcmp.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 11:51:12 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/15 13:16:11 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+	int diff;
+
+	i = 0;
+	diff = 0;
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = (s1[i] - s2[i]);
+			return (diff);
+		}
+		else
+			i++;
+	}
+	return (diff);
+}
+
+int		main(void)
+{
+	char str1[] = "abc";
+	char str2[] = "abcd";
+
+	ft_strcmp(str1, str2);
+	return (0);
+}
