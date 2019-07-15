58f66b486f28abbbf5c780f7b70d3e26031c6418
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-15 21:33:55 +0200

    ft_strncat

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 21:29:00 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/15 21:33:29 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+char	ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	int				j;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while ((src[i] != '\0') && (i < nb))
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	return (*dest);
+}
+
+int		main(void)
+{
+	unsigned int	nb;
+	char			str1[25] = "abcdefg";
+	char			str2[] = "abcdefghijkl";
+
+	nb = 6;
+	ft_strncat(str1, str2, nb);
+	return (0);
+}
